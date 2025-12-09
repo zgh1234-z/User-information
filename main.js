e2fdf870c101d79cddf535f7e260b6160d53dfe8
zgh <ghahremans908@gmil.com>
2025-12-09 10:19:30 +0330

    add the Text and user

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const name = prompt("نام خود را وارد کنید ");
-const age = prompt("سن خود را وارد کنید ");
-const emaile = prompt("ایمیل را وارد کنید ");
-
-let User = {
-  // Name: name,
-  Age: age,
-  Emaile: emaile,
+let user = {
+  Name: zahra,
+  Age: 18,
+  Email: zahra18gimal.come,
+  Text() {
+    ` ${this.Email} وایمیل شما ${this.Age}خوش امدید به سایت . سن شما ${this.Name} `;
+  },
 };
-
-document.getElementById("U").innerhtml = User.Age;
-console.log(User);
+console.log(user.Text);
+document.getElementById("U").innerHTML = user.Text();
